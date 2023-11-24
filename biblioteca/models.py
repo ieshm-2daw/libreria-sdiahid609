cc73cc8d905c0e821442dd79af3d1aec4906ba9d
sdiahid609 <xsdiahid609@ieshnosmachado.org>
2023-11-24 13:01:00 +0100

    models.py

--- biblioteca/models.py
+++ biblioteca/models.py
@@ -3,37 +3,46 @@ from django.contrib.auth.models import AbstractUser
 
 class Usuario(AbstractUser):
     dni = models.CharField(max_length=9)
-    direccion = models.EmailField()
+    direccion = models.CharField(max_length=20)
     telefono = models.IntegerField()
-    pass
     def __str__(self):
         return self.dni
 
 class Autor(models.Model):
     nombre = models.CharField(max_length=20)
     biografia = models.TextField()
-    foto = models.ImageField()
+    foto = models.ImageField() 
+    def __str__(self):
+        return self.nombre
 
 class Editorial(models.Model):
     nombre = models.CharField(max_length=20)
     direccion = models.CharField(max_length=30)
     sitioWeb = models.URLField()
+    def __str__(self):
+        return self.nombre
 
 
 class Libro(models.Model):
     titulo = models.CharField(max_length=30)
-    autor = Autor
-    editorial = Editorial
+    autor = models.ManyToManyField(Autor)
+    editorial = models.ForeignKey(Editorial, on_delete=models.CASCADE)
     fechaPublicacion = models.DateField()
     genero = models.CharField(max_length=10)
     isbn = models.CharField(max_length=20)
     resumen = models.TextField()
-    disponibilidad = models.CharField(max_length=20)
+    disponibilidad = models.CharField(max_length=20, choices=(
+        ('disponible', 'Disponible'),
+        ('prestado', 'Prestado'),
+        ('proceso', 'En proceso de prestamo'),
+        ))
     portada = models.ImageField()
+    def __str__(self):
+        return self.titulo
 
 class Prestamo(models.Model):
-    libroPrestado = Libro
+    libroPrestado = models.ForeignKey(Libro, on_delete=models.CASCADE)
     fechaPrestamo = models.DateField()
     fechaDevolucion = models.DateField()
-    usuarioPrestado = Usuario
+    usuarioPrestado = models.ForeignKey(Usuario, on_delete=models.CASCADE)
     estadoPrestamo = models.CharField(max_length=20)
